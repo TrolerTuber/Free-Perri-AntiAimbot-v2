948d95157306f0c85ad33a653ae5925c67064d48
TrolerTuber <94766847+TrolerTuber@users.noreply.github.com>
2024-08-14 11:33:11 +0200

    Update main.lua

--- Perri_AntiAim/client/main.lua
+++ Perri_AntiAim/client/main.lua
@@ -1,45 +1,51 @@
+local firstSpawn = true
 
+AddEventHandler('playerSpawned', function()
+    if firstSpawn then
+        firstSpawn = false
 
-CreateThread(function()
-    local test = false
-    local PlayerId = PlayerId()
-    local ped = PlayerPedId()
-    local Press = IsControlPressed
-    local Aiming = IsPlayerFreeAiming
-    while true do
-    
-        if Press(0, 25) or Aiming(PlayerId) then
-            Wait(500)
-            if Press(0, 25) or Aiming(PlayerId) then
-                test = true
-            else
-
-                test = false
-            end
-        end
-        --
-        if IsPedReloading(ped) or not Aiming(PlayerId) then
-            test = false
-        else
-            test = true
-
-        end
-        --
-        Wait(750)
 
         CreateThread(function()
-            while test do
-                
-                if Press(0, 25) then
-                    SetGameplayCamRelativeRotation(0)
+            local test = false
+            local player = PlayerId()
+            local ped = PlayerPedId()
+            local ControlPressed = IsControlPressed
+            local Aiming = IsPlayerFreeAiming
+
+            while true do
+                if ControlPressed(0, 25) or Aiming(player) then
+                    Wait(500)
+                    if ControlPressed(0, 25) or Aiming(player) then
+                        test = true
+
+                    else
+                        test = false
+
+                    end
+                end
+
+                if IsPedReloading(ped) or not Aiming(player) then
+                    test = false
                 else
 
-                    test = false
+                    test = true
                 end
-                Wait(0)
+
+                Wait(750)
+
+                CreateThread(function()
+                    while test do
+                        if ControlPressed(0, 25) then
+                            SetGameplayCamRelativeRotation(0, 0, 0)
+                        else
+
+                            test = false
+                        end
+                        
+                        Wait(0)
+                    end
+                end)
             end
         end)
-
     end
 end)
-
